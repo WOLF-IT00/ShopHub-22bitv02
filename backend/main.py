da574454a543f680939182d0ca9fdca734c05f8a
WOLF-IT00 <zxnhankl@gmail.com>
2026-06-27 14:37:51 +0700

    Complete Session 06 FastAPI Product API

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,88 @@
-function App() {
-  return (
-    <>
-      <h1>ShopHub</h1>
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-      <h2>Nhóm thực hiện</h2>
+app = FastAPI() 
 
-      <ul>
-        <li>Lê Thanh Trọng - 2200009170</li>
-        <li>Nguyễn Mai Tú Mẫn - 2200006620</li>
-        <li>Nguyễn Hoàng Pháp Bảo - 2200006550</li>
-      </ul>
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
 
-      <h3>Session 01 - 08/06/2026</h3>
-    </>
-  );
-}
+products = [
+    {
+        "id": 1,
+        "name": "iPhone 15",
+        "price": 25000000
+    },
+    {
+        "id": 2,
+        "name": "Samsung S25",
+        "price": 22000000
+    },
+    {
+        "id": 3,
+        "name": "Xiaomi 15",
+        "price": 15000000
+    }
+]
 
-export default App;
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to ShopHub API"
+    }
+
+
+@app.get("/products")
+def get_products():
+    return products
+
+@app.get("/products/{id}")
+def get_product(id: int):
+    for product in products:
+        if product["id"] == id:
+            return product
+
+    return {
+        "message": "Product not found"
+    }
+
+@app.post("/products")
+def create_product(product: Product):
+    products.append(product.dict())
+
+    return {
+        "message": "Product created successfully",
+        "product": product
+    }
+
+@app.put("/products/{id}")
+def update_product(id: int, updated_product: Product):
+    for index, product in enumerate(products):
+        if product["id"] == id:
+            products[index] = updated_product.dict()
+
+            return {
+                "message": "Product updated successfully",
+                "product": updated_product
+            }
+
+    return {
+        "message": "Product not found"
+    }
+
+@app.delete("/products/{id}")
+def delete_product(id: int):
+    for index, product in enumerate(products):
+        if product["id"] == id:
+            deleted_product = products.pop(index)
+
+            return {
+                "message": "Product deleted successfully",
+                "product": deleted_product
+            }
+
+    return {
+        "message": "Product not found"
+    }
